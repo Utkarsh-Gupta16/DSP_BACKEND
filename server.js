d3adc0a78572550b0e5edf4cadae4e9f18b8a524
Utkarsh-Gupta16 <utkarshgh16@gmail.com>
2025-05-01 14:57:38 +0530

    Updated on 1 May

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.NODE_ENV === "production"
-        ? "http://localhost:5000/api/users/google/callback"
-        : "http://localhost:5000/api/users/google/callback",
+        ? "https://dsp-backend.onrender.com/api/users/google/callback"
+        : "https://dsp-backend.onrender.com/api/users/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
